2185dfdd106b095f14d54b27773c17fa3573f00b
TechShreyash <techshreyash123@gmail.com>
2025-01-28 23:58:01 +0530

    Update

--- clean.py
+++ clean.py
@@ -2,15 +2,18 @@ import os
 
 def delete_non_cpp_files(base_dir, script_name):
     # Define folders to ignore
-    ignored_folders = {'.github', '.vscode', '.git'}
+    ignored_folders = { '.vscode', '.git'}
+    
+    # Define files to skip
+    ignored_files = {'.gitignore', 'README.md', script_name}
     
     for root, dirs, files in os.walk(base_dir):
         # Skip the ignored directories
         dirs[:] = [d for d in dirs if d not in ignored_folders]
         
         for file in files:
-            # Skip the script itself and keep only `.cpp` files
-            if file != script_name and not file.endswith('.cpp'):
+            # Skip the ignored files and keep only `.cpp` files
+            if file not in ignored_files and not file.endswith('.cpp'):
                 file_path = os.path.join(root, file)
                 try:
                     os.remove(file_path)
