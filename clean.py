dc21aea1554c9d0025cb1fb9f338b57acd0de062
TechShreyash <techshreyash123@gmail.com>
2025-02-25 16:40:46 +0530

    Added Debug Template

--- clean.py
+++ clean.py
@@ -1,25 +1,27 @@
 import os
 
+
 def delete_non_cpp_files(base_dir, script_name):
     # Define folders to ignore
-    ignored_folders = { '.vscode', '.git'}
-    
+    ignored_folders = {".vscode", ".git"}
+
     # Define files to skip
-    ignored_files = {'.gitignore', 'README.md', script_name}
-    
+    ignored_files = {".gitignore", "debug.hpp", "README.md", script_name}
+
     for root, dirs, files in os.walk(base_dir):
         # Skip the ignored directories
         dirs[:] = [d for d in dirs if d not in ignored_folders]
-        
+
         for file in files:
             # Skip the ignored files and keep only `.cpp` files
-            if file not in ignored_files and not file.endswith('.cpp'):
+            if file not in ignored_files and not file.endswith(".cpp"):
                 file_path = os.path.join(root, file)
                 try:
                     os.remove(file_path)
                     print(f"Deleted: {file_path}")
                 except Exception as e:
                     print(f"Failed to delete {file_path}: {e}")
+
 
 if __name__ == "__main__":
     current_directory = os.getcwd()
