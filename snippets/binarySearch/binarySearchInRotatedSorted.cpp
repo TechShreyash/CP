490bbf5b0ccd23dac58df8c138d15c7da978e831
TechShreyash <techshreyash123@gmail.com>
2025-03-20 20:01:44 +0530

    TLE Level 1 Contest 2, Codeforce Educational div2, Codechef starters, Leetcode Biweekly

---
+++ snippets/binarySearch/binarySearchInRotatedSorted.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool search(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    int l = 0, r = n - 1;
+
+    while (l <= r)
+    {
+        cout << l << " " << r << "\n";
+        int m = (l + r) / 2;
+
+        if (nums[m] == target)
+        {
+            return true;
+        }
+        else if ((nums[l] == nums[m]) && (nums[r] == nums[m]))
+        {
+            l++;
+            r--;
+        }
+        else if (nums[l] <= nums[m])
+        {
+            // left sorted
+            if ((target <= nums[m]) && (nums[l] <= target))
+            {
+                r = m - 1;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+        else if (nums[r] >= nums[m])
+        {
+            // right sorted
+            if ((target <= nums[r]) && (target >= nums[m]))
+            {
+                l = m + 1;
+            }
+            else
+            {
+                r = m - 1;
+            }
+        }
+    }
+
+    return false;
+}
